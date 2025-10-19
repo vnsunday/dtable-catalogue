3f4fe7e00a083f0df21ca6f10f75330b30a5ec20
vn_sunday <duc.van@corp.cloud.com>
2025-10-19 12:14:43 +0700

    declarations

---
+++ src/declarations.hpp
@@ -0,0 +1,13 @@
+#ifndef DTABLE_DATALOGUE_DECLARATION_H_
+#define DTABLE_DATALOGUE_DECLARATION_H_
+
+#include <string>
+#include <vector>
+#include <map>
+
+unsigned char component_variable; // 
+unsigned char integral;  // Integral 
+
+
+
+#endif
