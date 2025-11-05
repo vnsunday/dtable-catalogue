f0537c9b33ca8674cd6bbc0a659c68a49b343044
vnsunday <spidervn@spidervn.com>
2025-11-05 19:39:15 +0700

    declarations

---
+++ src/declarations.cpp
@@ -0,0 +1,8 @@
+#include "declarations.hpp"
+#include <dynsocc/fundamental/stdex/algorithm.hpp>
+
+int Initialize() {
+	mpGraph["name"] = "dtable-catalogue";
+	return 0;
+}
+
--- src/declarations.hpp
+++ src/declarations.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 
+
 #define LB_SUM "sum"
 #define LB_ELEMENT "element"
 #define LB_RANGE "range"
@@ -13,11 +14,17 @@
 #define LB_CONSTANT "constant" // Number - exchange value 
 #define LB_VARIABLE "variable" 
 
+#define LB_STATISTIC "statistic" // A calculation from data 
+#define LB_SAMPLE "sample"
+#define LB_DISCRETE "discrete"
+#define LB_CONTINUOUS "continuous"
+
 unsigned char component_variable;   // 
 unsigned char integral;             // Integral 
 
+std::map<std::string, std::string> mpGraph;
 
-/* */
-
+/* Setup everything */
+int Initialize();
 
 #endif
