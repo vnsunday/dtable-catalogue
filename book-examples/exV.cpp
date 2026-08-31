6d81013cbde2d9d0b19f7a683dcd8edf79dcd86b
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-31 20:25:24 +0700

    significance level

--- book-examples/exV.cpp
+++ book-examples/exV.cpp
@@ -4,11 +4,38 @@
 
 void ex_1_1() {
     Meta M;
+    const char* szM[][2] = {
+        { "Null", "" }, 
+        { "Alternative", "" }, 
+        { "Power", "" } 
+    };
     M.Graph("Model", {});
     M.Graph("Substitution", {});
 }
 
-int main()
-{
+void ex_2_1() {
+    Meta M;
+    const char* szIS[][2] = {
+        { "Average", "8500" },
+        { "ActualAverage", "8900" },
+        { "Distribution", "Normal" },
+        { "sigma", "2600" },
+        { "SignificanceLevel", "0.05"}
+    };
+    M.Graph("Model", szIS);
+}
+
+void ex_2_5() {
+    Meta M;
+    // M.Graph("", );
+    /*
+    const char* szM[][2] = {
+        { "", "" }, 
+        { "", "" }
+    };
+    */
+}
+
+int main() {
     return 0;
 }
